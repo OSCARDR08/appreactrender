b9c7d446610fcee5f63a9a6ff9c987ad1abcc485
OSCARDR08 <33297176+OSCARDR08@users.noreply.github.com>
2023-02-18 13:09:16 -0600

    Add files via upload

---
+++ src/Employee.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+
+export class Employee extends Component{
+
+    render(){
+
+        return (<div>
+                 <h3> This is Employee page  </h3>
+                </div>)
+     }
+
+}
+
---
+++ src/Variables.js
@@ -0,0 +1,6 @@
+export const variables={
+
+    API_URL:"https://testhn02.onrender.com//api/",
+    PHOTO_URL:"https://testhn02.onrender.com/api/Photos/"
+
+}
